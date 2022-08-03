d0e2e8bfa1dda9157c9bbf4a04c3bf0486f3d461
tamarOstry <tamar32410@gmail.com>
2022-08-03 14:41:37 +0300

    g

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/users'),
+    UserModule,
+  ],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [UserController],
   providers: [UserService],
 })
