3dc84cd94bb31b351416c6d45597e1e42a013ea0
naamafrank1 <naama0556772275@gmail.com>
2022-08-04 11:27:11 +0300

    eslint-disable prettier/prettier

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Get,Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import * as mongoose from 'mongoose';
 export const UserSchema = new mongoose.Schema({
     phone: { type: String },
