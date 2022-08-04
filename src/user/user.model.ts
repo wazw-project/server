6f8c4fdf1c8dbcefd59a89cd96a53a02e65cc7a3
tamarOstry <tamar32410@gmail.com>
2022-08-04 11:04:27 +0300

    first commit

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 export const UserSchema = new mongoose.Schema({
     phone: { type: String },
     age: { type: Number },
-    name: { type: String },
+    name: { type: String ,required: true},
 });
 export interface User {
     phone: string;
